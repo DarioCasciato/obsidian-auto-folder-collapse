25846dd2811a2f4ca6672efbfc87a6b6865dd253
Dario Casciato <93987804+DarioCasciato@users.noreply.github.com>
2024-07-18 23:32:34 +0200

    add reload functionality if not working

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
 const { Plugin } = require('obsidian');
 
-
-function delay(time)
-{
+function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
-
-module.exports = class CollapseSubfoldersPlugin extends Plugin
-{
-    onload()
-    {
+module.exports = class CollapseSubfoldersPlugin extends Plugin {
+    onload() {
         console.log('Loading Collapse Subfolders Plugin');
 
-        const observer = new MutationObserver((mutations) =>
-        {
-            mutations.forEach((mutation) =>
-            {
-                if (mutation.attributeName === 'class')
-                {
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.attributeName === 'class') {
                     const target = mutation.target;
-                    if (target.classList.contains('is-collapsed'))
-                    {
+                    if (target.classList.contains('is-collapsed')) {
                         this.handleFolderCollapse(target);
                     }
                 }
@@ -29,10 +20,8 @@ module.exports = class CollapseSubfoldersPlugin extends Plugin
         });
 
         const fileExplorer = document.querySelector('.workspace-leaf-content');
-        if (fileExplorer)
-        {
-            observer.observe(fileExplorer,
-            {
+        if (fileExplorer) {
+            observer.observe(fileExplorer, {
                 attributes: true,
                 subtree: true,
                 attributeFilter: ['class'],
@@ -40,38 +29,43 @@ module.exports = class CollapseSubfoldersPlugin extends Plugin
         }
 
         this.observer = observer;
+
+        // Add a command to reload the plugin
+        this.addCommand({
+            id: 'reload-collapse-subfolders-plugin',
+            name: 'Reload Collapse Subfolders Plugin',
+            callback: () => this.reloadPlugin()
+        });
     }
 
-    onunload()
-    {
+    onunload() {
         console.log('Unloading Collapse Subfolders Plugin');
-        if (this.observer)
-        {
+        if (this.observer) {
             this.observer.disconnect();
         }
     }
 
-    handleFolderCollapse(folder)
-    {
+    async reloadPlugin() {
+        console.log('Reloading Collapse Subfolders Plugin');
+        this.onunload();
+        await delay(100); // Ensure there's a slight delay between unload and load
+        this.onload();
+    }
+
+    handleFolderCollapse(folder) {
         const subfolders = folder.querySelectorAll('.nav-folder-children .nav-folder');
         const collapsePromises = Array.from(subfolders).map(subfolder => this.collapseSubfolder(subfolder));
 
-        Promise.all(collapsePromises).then(() =>
-        {
+        Promise.all(collapsePromises).then(() => {
             console.log('All subfolders collapsed');
         });
     }
 
-    collapseSubfolder(subfolder)
-    {
-        return new Promise(async resolve =>
-        { // Note the use of async here
-            const observer = new MutationObserver(mutations =>
-            {
-                mutations.forEach(mutation =>
-                {
-                    if (mutation.target.classList.contains('is-collapsed'))
-                    {
+    collapseSubfolder(subfolder) {
+        return new Promise(async resolve => {
+            const observer = new MutationObserver(mutations => {
+                mutations.forEach(mutation => {
+                    if (mutation.target.classList.contains('is-collapsed')) {
                         observer.disconnect();
                         resolve();
                     }
@@ -81,13 +75,10 @@ module.exports = class CollapseSubfoldersPlugin extends Plugin
             observer.observe(subfolder, { attributes: true, attributeFilter: ['class'] });
 
             const collapseIcon = subfolder.querySelector('.nav-folder-collapse-indicator');
-            if (collapseIcon && !subfolder.classList.contains('is-collapsed'))
-            {
+            if (collapseIcon && !subfolder.classList.contains('is-collapsed')) {
                 collapseIcon.click();
                 await delay(100);
-            }
-            else
-            {
+            } else {
                 resolve();
             }
         });
