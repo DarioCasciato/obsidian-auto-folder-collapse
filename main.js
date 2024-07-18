096f9ace52c87f3dda4a55d2779a794db4772a93
Dario Casciato <dario.casciato@dormakaba.com>
2024-07-18 08:43:42 +0200

    Final Version of Plugin

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const { Plugin } = require('obsidian');
 
-module.exports = class CollapseSubfoldersPlugin extends Plugin {
-    onload() {
+module.exports = class CollapseSubfoldersPlugin extends Plugin
+{
+    onload()
+    {
         console.log('Loading Collapse Subfolders Plugin');
 
-        const observer = new MutationObserver((mutations) => {
-            mutations.forEach((mutation) => {
-                if (mutation.attributeName === 'class') {
+        const observer = new MutationObserver((mutations) =>
+        {
+            mutations.forEach((mutation) =>
+            {
+                if (mutation.attributeName === 'class')
+                {
                     const target = mutation.target;
-                    if (target.classList.contains('is-collapsed')) {
+                    if (target.classList.contains('is-collapsed'))
+                    {
                         this.handleFolderCollapse(target);
                     }
                 }
@@ -16,8 +22,10 @@ module.exports = class CollapseSubfoldersPlugin extends Plugin {
         });
 
         const fileExplorer = document.querySelector('.workspace-leaf-content');
-        if (fileExplorer) {
-            observer.observe(fileExplorer, {
+        if (fileExplorer)
+        {
+            observer.observe(fileExplorer,
+            {
                 attributes: true,
                 subtree: true,
                 attributeFilter: ['class'],
@@ -27,39 +35,33 @@ module.exports = class CollapseSubfoldersPlugin extends Plugin {
         this.observer = observer;
     }
 
-    onunload() {
+    onunload()
+    {
         console.log('Unloading Collapse Subfolders Plugin');
-        if (this.observer) {
+        if (this.observer)
+        {
             this.observer.disconnect();
         }
     }
 
-    handleFolderCollapse(folder) {
-        console.log('Folder collapsed:', folder);
+    handleFolderCollapse(folder)
+    {
         const subfolders = folder.querySelectorAll('.nav-folder-children .nav-folder');
-        subfolders.forEach((subfolder) => {
-            if (!subfolder.classList.contains('is-collapsed')) {
-                console.log('Collapsing subfolder:', subfolder);
-                subfolder.classList.add('is-collapsed');
-                this.collapseSubfolderContents(subfolder);
-                console.log('Collapsed subfolder:', subfolder);
-            } else {
-                console.log('Subfolder already collapsed:', subfolder);
+        subfolders.forEach((subfolder) =>
+        {
+            if (!subfolder.classList.contains('is-collapsed'))
+            {
+                this.collapseSubfolder(subfolder);
             }
         });
     }
 
-    collapseSubfolderContents(subfolder) {
-        const subfolderChildren = subfolder.querySelector('.nav-folder-children');
-        if (subfolderChildren) {
-            subfolderChildren.style.display = 'none';
-        }
+    collapseSubfolder(subfolder)
+    {
         const collapseIcon = subfolder.querySelector('.nav-folder-collapse-indicator');
-        if (collapseIcon) {
-            collapseIcon.classList.add('is-collapsed');
+        if (collapseIcon)
+        {
+            collapseIcon.click(); // Simulate click to collapse
         }
-        console.log('Collapsed contents of subfolder:', subfolder);
-        console.log('Subfolder display style:', subfolderChildren ? subfolderChildren.style.display : 'no children');
-        console.log('Subfolder collapse icon class:', collapseIcon ? collapseIcon.classList : 'no icon');
     }
 };
